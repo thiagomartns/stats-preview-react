7ea790dc7477ccebf428f46c704d764b0d323981
thiago <tmg3.mn@gmail.com>
2023-04-19 10:41:36 -0300

    Add JSX and styles to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import './App.scss';
+import ImageDesktop from './assets/img/image-header-desktop.jpg'
 
 function App() {
 
   return (
     <div className="App">
-      hello world
+      <div className="app-content">
+        <div className="app-content--info">
+          <div className="title-and-parag">
+            <h1 className="info-title">Get <span>insights</span> that help your business grow.</h1>
+            <p className="info-parag">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+          </div>
+          <div className="info-stats">
+            <div className="stats-container">
+              <h1 className='stats-container--number'>10k+</h1>
+              <p className="stats-container--type">companies</p>
+            </div>
+            <div className="stats-container">
+              <h1 className='stats-container--number'>314</h1>
+              <p className="stats-container--type">templates</p>
+            </div>
+            <div className="stats-container">
+              <h1 className='stats-container--number'>12m+</h1>
+              <p className="stats-container--type">queries</p>
+            </div>
+          </div>
+        </div>
+        <div className="app-content--img">
+          <div className="overlay"></div>
+        </div>
+      </div>
     </div>
   )
 }
